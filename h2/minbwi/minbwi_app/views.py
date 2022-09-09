25ad52d95c10f819ad3d6ffa8877b8a750b9113f
Jessen01 <pragasenmun@gmail.com>
2022-09-09 15:16:34 +0400

    update

--- h2/minbwi/minbwi_app/views.py
+++ h2/minbwi/minbwi_app/views.py
@@ -1,11 +1,16 @@
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Booking, Restaurant
+from .models import Booking, Restaurant,Region
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    y = Restaurant.objects.all()
+    y1 = Region.objects.all()
+    context = {"Restaurants":y,"Regions":y1}
+    return render(request,'index.html',context)
+    
 
 def addbooking(request):
     if(request.method == "POST"): 
@@ -13,10 +18,10 @@ def addbooking(request):
         request.POST.get('region')
         request.POST.get('seat')
         request.POST.get('message')
-        print( request.POST.get('name'))
+        print( request.POST.get('name'))        
         print(request.POST.get('region'))
-        resto =Restaurant.objects.filter(Restaurant_name='Le Pain Maison')[0]
-        x = Booking(Restaurant_booking =resto,number_of_seats=request.POST.get('seat'),message=request.POST.get('message'))
+        resto =Restaurant.objects.filter(Restaurant_name=request.POST.get('restaurant_name'))[0]
+        x = Booking(Restaurant_booking =resto,number_of_seats=request.POST.get('seat'),message=request.POST.get('message'),username=request.POST.get('name'))
         x.save()
 
         return HttpResponse("Your booking has been saved")
